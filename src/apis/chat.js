657cc0b377576d0f3f9510a41850c93c14dfa4fe
tomer bezalel <tomerb37@gmail.com>
2020-11-12 16:17:03 +0200

    Prod url

--- src/apis/chat.js
+++ src/apis/chat.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    //baseURL: 'https://tomer-chat-server.herokuapp.com'
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://tomer-chat-server.herokuapp.com'
+    //baseURL: 'http://localhost:5000'
 });
